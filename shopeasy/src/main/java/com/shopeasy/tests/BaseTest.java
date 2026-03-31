f1f6c86162bdcf7d49d75658eaf1dc2d434376b5
Huu An <phamhuuan@gmail.com>
2026-03-31 15:20:23 +0700

    ci: add matrix strategy for parallel Chrome + Firefox

--- shopeasy/src/main/java/com/shopeasy/tests/BaseTest.java
+++ shopeasy/src/main/java/com/shopeasy/tests/BaseTest.java
@@ -4,15 +4,22 @@ import com.shopeasy.driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
     protected static final String BASE_URL = "https://www.saucedemo.com";
 
     @BeforeMethod
-    public void setUp() {
-        String browser = System.getProperty("browser", "chrome");
-        driver = DriverFactory.createDriver(browser);
+    @Parameters({"browser"})
+    public void setUp(@Optional("chrome") String browser) {
+        // Ưu tiên System Property từ Maven (-Dbrowser=)
+        // Nếu không có thì dùng giá trị từ testng.xml
+        String browserToUse = System.getProperty("browser", browser);
+
+        System.out.println("[BaseTest] Khởi động browser: " + browserToUse);
+        driver = DriverFactory.createDriver(browserToUse);
         driver.get(BASE_URL);
     }
 
